e074006e486c1bf698201ac8cd42b60736d358a7
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-05-02 16:33:46 +0200

    💫 Add extra Request Values

--- views/components/location.js
+++ views/components/location.js
@@ -1,7 +1,12 @@
 export default function renderLocation(location) {
   return `
     <li class="location-item">
-      <button hx-post="/places" hx-target="#available-locations" hx-swap="outerHTML">
+      <button 
+        hx-post="/places" 
+        hx-vals='{"locationId": "${location.id}"}'  
+        hx-target="#available-locations" 
+        hx-swap="outerHTML"
+      >
         <img src="${`/images/${location.image.src}`}" alt="${location.image.alt}" />
         <h3>${location.title}</h3>
       </button>
