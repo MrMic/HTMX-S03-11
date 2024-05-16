dab42a01e85de66a0bc596527ed7c27f2a9a4e9a
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-05-16 15:39:52 +0200

    🪛 Update the UI without HTMX - w/ Custom JS

--- public/main.js
+++ public/main.js
@@ -1,5 +1,28 @@
-function showConfirmationFunction() {
-console.log("Showing Modal...");
+function showConfirmationFunction(event) {
+event.preventDefault();
+console.log("🪚 event:", event);
+
+const confirmationModal = `
+    <dialog class="modal">
+      <div id="confirmation">
+        <h2>Are you sure?</h2>
+        <p>Do you really want to ACTION this place?</p>
+        <div id="confirmation-actions">
+          <button id="action-no" className="button-text">
+            No
+          </button>
+          <button id="action-yes" className="button">
+            Yes
+          </button>
+        </div>
+      </div>
+    </dialog>
+  `;
+
+document.body.insertAdjacentHTML("beforeend", confirmationModal);
+
+const dialog = document.querySelector("dialog");
+dialog.showModal();
 }
 
 document.addEventListener("htmx:beforeRequest", showConfirmationFunction);
